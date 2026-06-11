d491dcacf3af6e02276906b3f11929f5313c849e
Aman <amank422815@gmail.com>
2026-06-11 18:05:55 +0530

    updated voice typing feature

--- src/components/VoiceControls.jsx
+++
@@ -1,8 +0,0 @@
-    // function VoiceControls({ listening, onStart, onStop }) {
-    //   return (
-    //     <button onClick={listening ? onStop : onStart}>
-    //       {listening ? "Stop Recording" : "Start Recording"}
-    //     </button>
-    //   );
-    // }
-    // export default VoiceControls;
